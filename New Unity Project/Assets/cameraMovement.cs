43eb8fab594b17bff995b232b1463133082abad2
Sharpenski <dobbst1994@gmail.com>
2016-03-07 17:29:07 +0000

    Not sure what's happening here

---
+++ New Unity Project/Assets/cameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMovement : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+	public float RotateSpeed = 30f;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Rotate (-Vector3.up * RotateSpeed * Time.deltaTime);
+		else if (Input.GetKey (KeyCode.RightArrow))
+			transform.Rotate (Vector3.up * RotateSpeed * Time.deltaTime);*/
+	}
+
+	void LateUpdate() {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ New Unity Project/Assets/colourMeIn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colourMeIn : MonoBehaviour {
+
+	public float detectionRange;
+	public bool closeEnough;
+	public Transform player;
+
+
+	// Use this for initialization
+	void Start () {
+		//Renderer rend = GetComponent<Renderer> ();
+		//rend.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (player.position);
+		Renderer rend = GetComponent<Renderer> ();
+		rend.material.color = Color.red;
+
+		closeEnough = false;
+
+		if (Vector3.Distance (player.position, transform.position) <= detectionRange) {
+			closeEnough = true;
+		}
+
+		if (closeEnough == true) {
+			rend = GetComponent<Renderer> ();
+			rend.material.color = Color.green;
+			Debug.Log ("Reached the Underground Train Station");
+		}
+	}
+}
